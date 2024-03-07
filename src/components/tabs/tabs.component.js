4a5480fde14c7c1ec64c95221d21efd940977e40
Hardtmuth <abramow-a.v@ya.ru>
2024-03-07 11:14:19 +0300

    Revert "buttons again"
    This reverts commit 380d777b4c64468cadc02f436d7b6b1f9b3acd66.

--- src/components/tabs/tabs.component.js
+++ src/components/tabs/tabs.component.js
@@ -100,7 +100,7 @@ class Tabs extends Component {
       }
 
       .nav {
-          color: #fff;1
+          color: #fff;
       }
 
       #panels {
@@ -216,7 +216,7 @@ class Tabs extends Component {
           display: inline-flex;
           align-items: center;
           padding: .4em .7em;
-          background: #004a5a;
+          background: #02212b;
           box-shadow: 0 4px rgba(4, 59, 72, 0.5), 0 5px 10px rgb(0 0 0 / 20%);
           border-radius: 2px;
           margin-bottom: .7em;
